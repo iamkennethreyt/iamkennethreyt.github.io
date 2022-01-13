eba0d58ccf3e4881c6689fc6a0f9941edbfe9a19
Kenneth Rey Tamis <36709201+iamkennethreyt@users.noreply.github.com>
2022-01-13 09:34:04 +0800

    Add files via upload

---
+++ js/language.js
@@ -0,0 +1,141 @@
+const outputTxt = {
+    english: {
+        name: "Kenneth Rey <span class='text-secondary'>Tamis</span>",
+        title: "DevOps / Full Stack Node.js Developer",
+        aboutMe: "About <span class='text-secondary'>Me</span>",
+        aboutMeDetails: "Let me tell you a few things...",
+        aboutMeParagraph: "I am a DevOps Engineer who has an outright understanding in the software development lifecycle, as well as to the various automation tools for developing digital pipelines, which gave a huge impact in my career. Aside from that, I am also a full stack JavaScript web developer specializing Node.js, React.js and other new JavaScript libraries and frameworks.</br>I get curious and excited to learn whenever there are new trends in web development field. As a matter of fact, I have many premium courses in <a class='text-secondary' target='blank' href='https://www.udemy.com/user/kenneth-rey-tamis-2/'> Udemy</a> in order for my knowledge to grow. I have good technical ability as well in developing business solutions management system",
+        workTitle0: "Project Leader",
+        workExp0: "May 2021-Present",
+        workDetails0: "I am directing the development team in the design, development, coding, testing, and debugging of developing dropshipping software for the amazon and Walmart sellers. I am also working closely with the production team to understand business requirements and help translate them into technical requirements for the development team.",
+        workTitle1: "Assistant Research Development Engineer",
+        workExp1: "May 2019-July 2021",
+        workDetails1: "I was previously a DevOps Engineer in SHARP Corporation in Japan overseeing the code releases of the App. Moreover, I became part of the team that develops a sign server for the user email of the Lenovo Corporation. In the present, I am working as an OSS engineer on the NOTH project in NEC.",
+        workTitle2: "Software Engineer",
+        workExp2: "May 2018-May 2019",
+        workDetails2: "Had been part of the Web Team and successfully developed the <a target='blank' class='text-secondary' href='http://www.autoterminal.co.nz/'>Autoterminal.co.nz</a> at that time. Was once a Fullstack Developer who coded from the backend using C# programming language, and React.js and JQuery for the front end.",
+        myWorks: "My<span class='text-secondary'>Works</span>",
+        myWorkDetails: "Check out some of my projects that can be accessed on the public...",
+        ee: "<i class='fas fa-eye'></i> E&E Construction Company Website",
+        teknolikha: "<i class='fas fa-eye'></i> Teknolikha Website",
+        epres: "<i class='fas fa-eye'></i> Electronic Prescription",
+        hanaph: "<i class='fas fa-eye'></i> HanaPh (Job Searching App)",
+        crud: "<i class='fas fa-eye'></i> CRUD app using Node.js Project",
+        login: "<i class='fas fa-eye'></i> Login app using Node.js project",
+        contactMe: "Contact <span class='text-secondary'>Me</span>",
+        contactMeDetails: "These are the means on how you can reach me...",
+        email: "<span class='text-secondary'>Email: </span> kennethreyt@gmail.com",
+        phone: "<span class='text-secondary'>Phone: </span> +63 966 356 8932",
+        address: "<span class='text-secondary'>Address: </span>Purok 3, Marikaban Santa Fe, Philippines, 6047",
+        links: "Links:",
+        home: "Home",
+        about: "About Me",
+        work: "My Works",
+        contact: "How to reach me",
+        download: "Download my Resume",
+        titleIndex: "Welcome To My Portfolio | Kenneth Rey Tamis",
+        titleAbout: "About Me | Kenneth Rey Tamis",
+        titleContact: "Contact Me | Kenneth Rey Tamis",
+        titleWork: "My Works | Kenneth Rey Tamis"
+    },
+    japanese: {
+        name: "ケネス・レイ・<span class='text-secondary'>タミス</span>",
+        title: "DevOps /フルスタックNode.js開発者",
+        aboutMe: "私につ <span class='text-secondary'>いて</span>",
+        aboutMeDetails: "いくつか教えてあげましょう",
+        aboutMeParagraph: "私はDevOpsエンジニアであり、ソフトウェア開発ライフサイクルと、デジタルパイプラインを開発するためのさまざまな自動化ツールを完全に理解しており、私のキャリアに大きな影響を与えました。それとは別に、Node.js、React.js、およびその他の新しいJavaScriptライブラリとフレームワークを専門とするフルスタックJavaScript Web開発者でもあります。</br> Web開発分野に新しいトレンドがあるときはいつでも知りたいと思います。実際のところ、知識を伸ばすために、<a class='text-secondary' target='blank' href='https://www.udemy.com/user/kenneth-rey-tamis-2/'>Udemy</a> には多くのプレミアムコースがあります。ビジネスソリューションマネジメントシステムの開発にも技術力がある",
+        workTitle0: "プロジェクトリーダー",
+        workExp0: "2021年5月-現在",
+        workDetails0: "私は、AmazonとWalmartの販売者向けのドロップシッピングソフトウェアの開発の設計、開発、コーディング、テスト、およびデバッグを開発チームに指示しています。また、制作チームと緊密に連携して、ビジネス要件を理解し、それを開発チームの技術要件に変換するのを支援しています。",
+        workTitle1: "アシスタント研究開発エンジニア",
+        workExp1: "2019年5月-現在",
+        workDetails1: "私は以前、日本のシャープ株式会社のDevOpsエンジニアで、アプリのコードリリースを監督していました。また、Lenovo Corporationのユーザーメール用のサインサーバーを開発するチームの一員になりました。現在、私はNECのNOTHプロジェクトでOSSエンジニアとして働いています。",
+        workTitle2: "ソフトウェアエンジニア",
+        workExp2: "2018年5月〜2019年5月",
+        workDetails2: "Webチームの一員で、当時は<a target='blank' class='text-secondary' href='http://www.autoterminal.co.nz/'>Autoterminal.co.nz</a>の開発に成功していました。かつてはC＃プログラミング言語を使用してバックエンドからコーディングし、フロントエンドにはReact.jsとJQueryをコーディングしていたFullstack Developerでした。",
+        myWorks: "私の<span class='text-secondary'>仕事</span>",
+        myWorkDetails: "公開でアクセスできる私のプロジェクトのいくつかをチェックしてください",
+        ee: "<i class='fas fa-eye'></i> E＆E Con​​struction Company ウェブサイト",
+        teknolikha: "<i class='fas fa-eye'></i> Teknolikha ウェブサイト",
+        epres: "<i class='fas fa-eye'></i> 電子処方箋",
+        hanaph: "<i class='fas fa-eye'></i> HanaPh（仕事検索アプリ",
+        crud: "<i class='fas fa-eye'></i> Node.jsプロジェクトを使用したCRUDアプリ",
+        login: "<i class='fas fa-eye'></i> Node.jsプロジェクトを使用したログインアプリ",
+        contactMe: "私に<span class='text-secondary'>連絡して</span>",
+        contactMeDetails: "これらはあなたが私に到達する方法の手段です",
+        email: "<span class='text-secondary'>Eメール: </span> kennethreyt@gmail.com",
+        phone: "<span class='text-secondary'>電話番号: </span> +63 966 356 8932",
+        address: "<span class='text-secondary'>住所: </span>Purok 3, Marikaban Santa Fe, Philippines, 6047",
+        links: "リンク:",
+        home: "ホームページ",
+        about: "私について",
+        work: "私のプロジェクト",
+        contact: "連絡方法",
+        download: "履歴書をダウンロード",
+        titleIndex: "私のポートフォリオへようこそ | ケネス・レイ・タミス",
+        titleAbout: "私について | ケネス・レイ・タミス",
+        titleContact: "連絡先の詳細 | ケネス・レイ・タミス",
+        titleWork: "私のプロジェクト | ケネス・レイ・タミス"
+    }
+}
+
+$("#lang-en").on("click", () => {
+    localStorage.setItem('lang', 'English');
+    window.location.reload()
+
+});
+
+$("#lang-jp").on("click", () => {
+    localStorage.setItem('lang', 'Japanese');
+    window.location.reload()
+});
+
+if (localStorage.getItem("lang") === "Japanese") {
+    $("#lang-jp").addClass("text-secondary");
+} else {
+    $("#lang-en").addClass("text-secondary");
+}
+
+const txt = (localStorage.getItem("lang") === "Japanese") ? outputTxt.japanese : outputTxt.english
+
+$("#name").html(txt.name);
+$("#title").text(txt.title);
+$("#aboutMe").html(txt.aboutMe);
+$("#aboutMeDetails").text(txt.aboutMeDetails);
+$("#aboutMeParagraph").html(txt.aboutMeParagraph);
+$("#workTitle0").text(txt.workTitle0);
+$("#workExp0").text(txt.workExp0);
+$("#workDetails0").html(txt.workDetails0);
+$("#workTitle1").text(txt.workTitle1);
+$("#workExp1").text(txt.workExp1);
+$("#workDetails1").html(txt.workDetails1);
+$("#workTitle2").text(txt.workTitle2);
+$("#workExp2").text(txt.workExp2);
+$("#workDetails2").html(txt.workDetails2);
+$("#myWorks").html(txt.myWorks);
+$("#myWorkDetails").text(txt.myWorkDetails);
+$("#ee").html(txt.ee);
+$("#teknolikha").html(txt.teknolikha);
+$("#epres").html(txt.epres);
+$("#hanaph").html(txt.hanaph);
+$("#crud").html(txt.crud);
+$("#login").html(txt.login);
+$("#contactMe").html(txt.contactMe);
+$("#contactMeDetails").text(txt.contactMeDetails);
+$("#email").html(txt.email);
+$("#phone").html(txt.phone);
+$("#address").html(txt.address);
+$("#links").text(txt.links);
+$("#home").text(txt.home);
+$("#about").text(txt.about);
+$("#contact").text(txt.contact);
+$("#download").text(txt.download);
+$("#work").text(txt.work);
+
+$("#titleIndex").text(txt.titleIndex);
+$("#titleAbout").text(txt.titleAbout);
+$("#titleContact").text(txt.titleContact);
+$("#titleWork").text(txt.titleWork);
+
+
+
